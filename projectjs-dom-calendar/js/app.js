6b94cad9a0335c10fc9983b14f8ca4090f5d0aa0
BagerManBG <toshko_1998@abv.bg>
2019-02-09 15:51:20 +0200

    Added append, prepend and delete methods to global lib object

--- projectjs-dom-calendar/js/app.js
+++ projectjs-dom-calendar/js/app.js
@@ -11,4 +11,17 @@ window.onload = () => {
   const ps = selectDOM('p');
   console.log(ps);
   console.log(ps.get(1));
+  console.log(ps.length());
+
+  /**
+   * Test for #1.2 task.
+   */
+  selectDOM('div')
+    .append('<b>I\'m BOLD</b>')
+    .prepend('<i>I\'m ITALIC</i>');
+
+  /**
+   * Test for #1.2 task.
+   */
+  selectDOM('i').get(0).delete();
 };
--- projectjs-dom-calendar/js/lib/js-dom.js
+++ projectjs-dom-calendar/js/lib/js-dom.js
@@ -11,15 +11,54 @@ class jsDOM {
   get(index) {
     const singleElement = this.elements[index];
     if (singleElement) {
-      const newObj = Object.assign({}, this);
+      const newObj = new jsDOM(this.selector);
       newObj.elements = [singleElement];
       return newObj;
     }
+  }
+
+  append(markup) {
+    this.elements.forEach(el => el.innerHTML += markup);
+    return this;
+  }
+
+  prepend(markup) {
+    this.elements.forEach(el => el.innerHTML = markup + el.innerHTML);
+    return this;
+  }
+
+  delete() {
+    this.elements.forEach(el => el.parentElement.removeChild(el));
+    this.elements = [];
+    return undefined;
   }
 
   length() {
     return this.elements.length;
   }
 }
+
+Object.prototype.clone = function () {
+  if (typeof this !== 'object' || this === null) {
+    return this;
+  }
+
+  const clonedObj = {};
+  const obj = this;
+
+  for (const prop in obj) {
+    if (obj.hasOwnProperty(prop)) {
+
+      let value = obj[prop];
+      if (value instanceof Object) {
+        value = value.clone();
+      }
+
+      clonedObj[prop] = value;
+    }
+  }
+
+  return clonedObj;
+};
 
 const selectDOM = selector => new jsDOM(selector);
