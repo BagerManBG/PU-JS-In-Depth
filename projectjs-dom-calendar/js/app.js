0afaf05f4db4a7cc68f2a5b01d73c5ec68a9ac08
BagerManBG <toshko_1998@abv.bg>
2019-02-12 01:07:45 +0100

    Finished tasks part #1

--- projectjs-dom-calendar/js/app.js
+++ projectjs-dom-calendar/js/app.js
@@ -8,20 +8,168 @@ window.onload = () => {
   /**
    * Test for #1.1 task.
    */
-  const ps = selectDOM('p');
-  console.log(ps);
-  console.log(ps.get(1));
-  console.log(ps.length());
+  (function () {
+    console.log('%c#######   Test for 1.1   #######', 'color: green');
+
+    const ps = selectDOM('p');
+    console.log(ps);
+    console.log(ps.get(1));
+    console.log(ps.length());
+
+    console.log();
+  })();
 
   /**
    * Test for #1.2 task.
    */
-  selectDOM('div')
-    .append('<b>I\'m BOLD</b>')
-    .prepend('<i>I\'m ITALIC</i>');
+  (function () {
+    console.log('%c#######   Test for 1.2   #######', 'color: green');
+
+    selectDOM('div')
+      .append('<b>I\'m BOLD</b>')
+      .prepend('<i>I\'m ITALIC</i>');
+
+    console.log();
+  })();
 
   /**
-   * Test for #1.2 task.
+   * Test for #1.3 task.
    */
-  selectDOM('i').get(0).delete();
+  (function () {
+    console.log('%c#######   Test for 1.3   #######', 'color: green');
+
+    selectDOM('i').get(0).delete();
+
+    console.log();
+  })();
+
+  /**
+   * Test for #1.4a task.
+   */
+  (function () {
+    console.log('%c#######   Test for 1.4a   #######', 'color: green');
+
+    const firstParClass = selectDOM('p#a')
+      .attr('data-c', 'ccc')
+      .attr('data-e', 'eee')
+      .attr('class');
+    console.log(firstParClass);
+
+    console.log();
+  })();
+
+  /**
+   * Test for #1.4b task.
+   */
+  (function () {
+    console.log('%c#######   Test for 1.4b   #######', 'color: green');
+
+    const firstPar = selectDOM('p#a').get(0);
+    console.log(firstPar.text());
+    firstPar.text('I am a paragraph and my text was changed.');
+    console.log(firstPar.text());
+
+    console.log();
+  })();
+
+  /**
+   * Test for #1.4c task.
+   */
+  (function () {
+    console.log('%c#######   Test for 1.4c   #######', 'color: green');
+
+    selectDOM('body').append('<div id="div-at-the-end">empty</div>');
+
+    const endDiv = selectDOM('#div-at-the-end');
+    console.log(endDiv.html());
+    endDiv.html('<b> I\'m no longer empty!</b>');
+    console.log(endDiv.html());
+
+    console.log();
+  })();
+
+  /**
+   * Test for #1.4d task.
+   */
+  (function () {
+    console.log('%c#######   Test for 1.4d   #######', 'color: green');
+
+    const allParagraphs = selectDOM('p');
+    const allDivs = selectDOM('div');
+
+    allParagraphs.css('color', 'red');
+    allDivs.css({
+      marginTop: '50px',
+      marginBottom: '50px',
+      fontSize: '14px',
+      width: '100%',
+    });
+
+    console.log();
+  })();
+
+  /**
+   * Test for #1.5a task.
+   */
+  (function () {
+    console.log('%c#######   Test for 1.5a   #######', 'color: green');
+
+    console.log(selectDOM('p').parent());
+
+    console.log();
+  })();
+
+  /**
+   * Test for #1.5b/c task.
+   */
+  (function () {
+    console.log('%c#######   Test for 1.5b/c   #######', 'color: green');
+
+    const el = selectDOM('div p').get(1);
+
+    console.log(el.siblings());
+    console.log(el.siblings('before'));
+    console.log(el.siblings('after'));
+
+    el.siblings().css('color', '#fac');
+    el.siblings('before').attr('data-sibling', 'before');
+    el.siblings('after').attr('data-sibling', 'after');
+
+    console.log();
+  })();
+
+  /**
+   * Test for #1.5d task.
+   */
+  (function () {
+    console.log('%c#######   Test for 1.5d   #######', 'color: green');
+
+    console.log(selectDOM('div').get(0).children());
+    selectDOM('div').get(0).children().css('fontFamily', 'Monotype Corsiva');
+
+    console.log();
+  })();
+
+  /**
+   * Test for #1.6 task.
+   */
+  (function () {
+    console.log('%c#######   Test for 1.6   #######', 'color: green');
+
+    selectDOM('body').prepend('<div class="chain"><p>Chaining</p></div>');
+    selectDOM('.chain')
+      .attr('id', 'chain')
+      .children()
+      .get(0)
+      .text('Chaining, New Text')
+      .css('fontSize', '20px')
+      .parent()
+      .css('textAlign', 'center')
+      .append('<p>New Element, next to chain</p>')
+      .append('<p>New Element, next to chain</p>')
+      .children()
+      .attr('data-chain', 'chain');
+
+    console.log();
+  })();
 };
--- projectjs-dom-calendar/js/lib/js-dom.js
+++ projectjs-dom-calendar/js/lib/js-dom.js
@@ -3,41 +3,295 @@
  * of a web page.
  */
 class jsDOM {
-  constructor(selector) {
-    this.selector = selector;
-    this.elements = document.querySelectorAll(selector);
+  /**
+   * @param selector
+   *
+   * Creates a jsDOM object by selecting
+   * the elements and saving them as an array.
+   */
+  constructor(selector = null) {
+    this.elements = [];
+    this.elements = Array.from(document.querySelectorAll(selector));
   }
 
+  /**
+   * @param selector
+   * @returns {jsDOM}
+   *
+   * Static method, which creates an empty jsDOM
+   * object without any elements.
+   */
+  static create(selector) {
+    return new jsDOM(selector);
+  }
+
+  /**
+   * @returns {number}
+   *
+   * Returns the number of elements in
+   * the jsDOM object.
+   */
+  length() {
+    return this.elements.length;
+  }
+
+  /**
+   * @param index
+   * @returns {jsDOM}
+   *
+   * Selects one of the elements of the currently
+   * selected ones by an index. Returns a jsDOM
+   * object, which has only one element selected.
+   */
   get(index) {
     const singleElement = this.elements[index];
     if (singleElement) {
-      const newObj = new jsDOM(this.selector);
-      newObj.elements = [singleElement];
-      return newObj;
+      const target = jsDOM.create();
+      target.elements = [singleElement];
+      return target;
     }
   }
 
-  append(markup) {
-    this.elements.forEach(el => el.innerHTML += markup);
-    return this;
+  /**
+   * @returns {Array}
+   *
+   * Gets the elements of the objects as
+   * a copied array.
+   */
+  toArray() {
+    return Array.from(this.elements);
   }
 
+  /**
+   * @returns {boolean}
+   *
+   * Checks if there is only one element in the set.
+   */
+  checkForSingleElement() {
+    const len = this.length();
+    if (len === 0) {
+      console.error('Set of elements is empty.');
+      return false;
+    }
+
+    if (this.length() !== 1) {
+      console.error('This method can be used only on a single element, please use the method "get" to pick an element.');
+      return false;
+    }
+
+    return true;
+  }
+
+  /**
+   * @returns {undefined}
+   *
+   * Removes all elements in the object
+   * and deletes the object itself.
+   */
+  delete() {
+    this.elements.forEach(el => el.parentElement.removeChild(el));
+    this.elements = [];
+    delete this;
+    return undefined;
+  }
+
+
+  /**
+   * @param markup
+   * @returns {jsDOM}
+   *
+   * Appends the markup at the beginning of the
+   * HTML for the elements.
+   */
   prepend(markup) {
     this.elements.forEach(el => el.innerHTML = markup + el.innerHTML);
     return this;
   }
 
-  delete() {
-    this.elements.forEach(el => el.parentElement.removeChild(el));
-    this.elements = [];
-    return undefined;
+  /**
+   * @param markup
+   * @returns {jsDOM}
+   *
+   * Appends the markup at the end of the
+   * HTML for the elements.
+   */
+  append(markup) {
+    this.elements.forEach(el => el.innerHTML += markup);
+    return this;
   }
 
-  length() {
-    return this.elements.length;
+  /**
+   * @param attr
+   * @param value
+   * @returns {jsDOM|String}
+   *
+   * Returns the value of an attribute if
+   * the value param is empty. Sets the selected
+   * attribute to the specified value if value is
+   * not empty.
+   */
+  attr(attr, value = null) {
+    if (value === null && this.checkForSingleElement()) {
+      return this.elements[0].attributes.getNamedItem(attr).value;
+    }
+    else {
+      this.elements.forEach(el => el.setAttribute(String(attr), String(value)));
+      return this;
+    }
+  }
+
+  /**
+   * @param value
+   * @returns {jsDOM|String}
+   *
+   * Return the text of an element if value is
+   * empty. Sets the text for the selected elements if
+   * value is not empty.
+   */
+  text(value = null) {
+    if (value === null && this.checkForSingleElement()) {
+      return this.elements[0].innerText;
+    }
+    else {
+      this.elements.forEach(el => el.innerText = String(value));
+      return this;
+    }
+  }
+
+  /**
+   * @param value
+   * @returns {jsDOM|String}
+   *
+   * Return the HTML of an element if value is
+   * empty. Sets the HTML for the selected elements if
+   * value is not empty.
+   */
+  html(value = null) {
+    if (value === null && this.checkForSingleElement()) {
+      return this.elements[0].innerHTML;
+    }
+    else {
+      this.elements.forEach(el => el.innerHTML = String(value));
+      return this;
+    }
+  }
+
+  /**
+   * @param props
+   * @param value
+   * @returns {jsDOM|Boolean}
+   *
+   * Sets the css for the selected elements.
+   * There are two ways to do so. First one is to change only one css rule.
+   * In that case the parameters are props for the property and value for the
+   * value. The other way is to change multiple rules. To do so, give an object
+   * with the correct maps as the only parameter.
+   */
+  css(props, value = null) {
+    const isValid = Boolean(props);
+    const isOneStyle = (typeof props === 'string' && typeof value === 'string');
+    const isMultipleStyles = (typeof props === 'object');
+    const isCorrect = (isValid && (isOneStyle || isMultipleStyles));
+
+    if (!isCorrect) {
+      console.error('Invalid arguments!');
+      return false;
+    }
+
+    if (isOneStyle) {
+      this.elements.forEach(el => el.style[props] = String(value));
+    }
+    else {
+      for (const prop in props) {
+        if (props.hasOwnProperty(prop)) {
+          this.elements.forEach(el => el.style[prop] = String(props[prop]));
+        }
+      }
+    }
+
+    return this;
+  }
+
+  /**
+   * @returns {jsDOM}
+   *
+   * Returns the parents of the selected elements.
+   */
+  parent() {
+    const parent = jsDOM.create();
+    parent.elements = this.elements.map(el => el.parentElement);
+    return parent;
+  }
+
+  /**
+   * @param position
+   * @returns {jsDOM|Boolean}
+   *
+   * Returns the siblings of the selected elements.
+   * No parameter means all siblings. Other options are
+   * 'before' and 'after'.
+   */
+  siblings(position = 'all') {
+    if (typeof position === 'string') {
+
+      const elements = this.toArray();
+      const children = this.parent().toArray().map(pr => pr.children);
+      let state = 'before';
+
+      const searchForSiblings = (stateGoal = null) => {
+        const sibling = jsDOM.create();
+        for (const childIndex in children) {
+
+          const len = children[childIndex].length;
+          for (let i = 0; i < len; i++) {
+
+            const stateCondition = (!stateGoal || stateGoal === state);
+            const propertyCondition = (children[childIndex].hasOwnProperty(i));
+            const invokerObjCondition = (children[childIndex][i].isSameNode(elements[childIndex]));
+
+            if (stateCondition && propertyCondition && !invokerObjCondition) {
+              sibling.elements.push(children[childIndex][i]);
+            }
+            else if (invokerObjCondition) {
+              state = 'after';
+            }
+          }
+        }
+
+        return sibling;
+      };
+
+      if (position === 'all') {
+        return searchForSiblings();
+      }
+      else if (position === 'before' || position === 'after') {
+        return searchForSiblings(position);
+      }
+    }
+
+    console.error('Bad position parameter (use "before", "after" or no parameter)!');
+    return false;
+  }
+
+  /**
+   * @returns {jsDOM}
+   *
+   * Returns the children of a selected element.
+   */
+  children() {
+    if (this.checkForSingleElement()) {
+      const children = jsDOM.create();
+      children.elements = Array.from(this.elements[0].children).filter((el, i) => !isNaN(Number(i)));
+      return children;
+    }
   }
 }
 
+/**
+ * @returns {Object}
+ *
+ * Helper method for copying an object.
+ */
 Object.prototype.clone = function () {
   if (typeof this !== 'object' || this === null) {
     return this;
@@ -61,4 +315,5 @@ Object.prototype.clone = function () {
   return clonedObj;
 };
 
-const selectDOM = selector => new jsDOM(selector);
+const
+  selectDOM = selector => new jsDOM(selector);
